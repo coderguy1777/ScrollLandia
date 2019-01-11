cc281710e0812180ff18d416e7f592bc3166f76a
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2019-01-10 20:50:10 -0500

    Add files via upload

--- Game Files/MainGameFile.py
+++ Game Files/MainGameFile.py
@@ -1,38 +1,109 @@
-from pygame import *
+import pygame
 
-(widdth, heiight) = (800, 600)
-screen = pygame.display.set_mode((widdth, heiight))
-pygame.display.set_caption("ScrollLandia")
-background = (222, 222, 22, 0)
-y = 40
-x = 40
-width = 40
-twoy = 400
-height = 40
-vel = 5
-screen.fill(background)
-pygame.display.flip()
 
-count = 20
+width = 960
+height = 540
+fps = 30
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+vec = pygame.Vector2
+
+pygame.init()
+pygame.mixer.init()
+title = pygame.display.set_caption("ScrollLandia")
+screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, x, y, w, h):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((w, h))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = width / 2
+        self.rect.bottom = height - 10
+        self.speedx = vec(0, 0)
+        self.speedy = vec(0, 0)
+        self.pos = 0
+
+    def update(self):
+        self.speedx = 0
+        self.speedy = 0
+        self.pos = self.rect.midbottom
+        keystate = pygame.key.get_pressed()
+
+        if keystate[pygame.K_d]:
+            self.speedx = 10
+
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 10
+        if keystate[pygame.K_a]:
+            self.speedx = -10
+        self.rect.x += self.speedx
+        if keystate[pygame.K_w]:
+            self.speedy = -10
+        if keystate[pygame.K_s]:
+            self.speedy = 10
+        self.rect.y += self.speedy
+        if self.rect.right > width:
+            self.rect.right = width
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.bottom > height:
+            self.rect.bottom = height
+        if self.rect.top < 0:
+            self.rect.top = 0
+
+    def test(self):
+        return self.speedx
+
+
+
+gamesprites = pygame.sprite.Group()
+platforms = pygame.sprite.Group()
+player = Player()
+gamesprites.add(player)
+plat = Platform(0, height - 40, width, 40)
+platforms.add(plat)
+
+
+
 running = True
-
 while running:
+    clock.tick(fps)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and x > vel:
-        x -= vel
-    if keys[pygame.K_RIGHT] and x < 740:
-        x += vel
-        count = count + 20
-    if keys[pygame.K_UP] and y > vel - 10:
-        y -= vel
-    if keys[pygame.K_DOWN] and y < 500 - vel:
-        y += vel
+    gamesprites.update()
+    platforms.update()
+    screen.fill(BLACK)
+    gamesprites.draw(screen)
+    platforms.draw(screen)
+    if player.rect.bottom > plat.rect.top:
+        player.rect.bottom = plat.rect.top
+    if player.rect.top < 10:
+        player.rect.top = 10
 
-    screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, (255, 255, 255, 255), (x, y, width, height))
-    screen2 = pygame.draw.rect(screen, (255, 255, 2, 255), (0, 530, 900, 90))
-    pygame.display.update()
+    if player.rect.top > plat.rect.bottom:
+        player.rect.top = 10
+    pygame.display.flip()
+    print(player.test())
+    print(player.rect.midbottom)
+
+pygame.quit()
--- Game Files/PlayerV.py
+++ Game Files/PlayerV.py
@@ -1,6 +1,6 @@
 import pygame
 
-class GamePlayer(object):
+class DisplayVals(pygame.display):
     def __init(self):
         self.playerpos = 0
         self.playerCamx = 40
@@ -10,6 +10,12 @@ class GamePlayer(object):
         self.healthloss = -10
         self.playerCamxchange = self.playerCamx - 40
         self.playerCamychange = self.playerCamy - 40
+        self.screen = pygame.display.get_active()
 
-    def test(self):
-        return self.name
+    def playermovement(self):
+        x = 0
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
